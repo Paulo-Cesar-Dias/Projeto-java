a5aebc09227a7d6de33e7823e1f62478556d4392
Thallyson Kauã dos Santos Vidal <vidal.santos@academico.ifrn.edu.br>
2023-02-03 15:19:41 -0300

    adicionando o projeto no repositorio

---
+++ BDSistGas/src/com/sistgas/controles/ManterCaixas.java
@@ -0,0 +1,77 @@
+
+package com.sistgas.controles;
+
+import java.sql.ResultSet;
+
+public class ManterCaixas {  
+    
+    public static ResultSet listarCaixas(){ 
+        string sql= "select * from caixa;"; 
+        ResultSet rs; 
+        
+        rs = AdaptadorBDSistGas.consultarBanco(sql) ;  
+        return rs;
+        
+    } 
+    
+    public static ResultSet getCaixa(String idcaixa) { 
+        String sql = "select * from caixa where idcaixa=" + idcaixa + ";"; 
+        ResultSet rs; 
+        
+        rs = AdaptadorBDSistGas.consultarBanco(sql) ;  
+        return rs;  
+    } 
+    
+    public static int adcionarCaixa(String descricao) { 
+        
+        int regInseridos = 0; 
+        
+        String insertSQL = "INSERT INTO caixa " + 
+                "(descricao) VALUES ("+ 
+                "'" +descricao+ "'" + 
+                ");"; 
+        
+        regInseridos = AdaptadorBDSistGas.atualizarBanco(InsertSQL) ;
+        
+        return regInseridos; 
+    }  
+    
+     public static int atualizarCaixa(String idCaixa, String descricao) { 
+        
+        int regAtualizados = 0; 
+        
+        String updateSQL = "UPDATE caixa SET " + 
+                
+                "descricao ='" +descricao+ "'" +  
+                " where idcaixa = " idCaixa+";";
+                
+        
+        regAtualizados = AdaptadorBDSistGas.atualizarBanco(updateSQL) ;
+        
+        return regAtualizados; 
+    }  
+     
+          public static int excluirCaixa(String idCaixa, String descricao) { 
+        
+            int regExcluidos = 0; 
+
+            String excluirSQL = "DELETE FROM caixa " + 
+
+                 " where idcaixa = " idCaixa+";";
+
+
+            regExcluidos = AdaptadorBDSistGas.atualizarBanco(excluirSQL) ;
+
+            return regExcluidos; 
+        } 
+          
+          
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ BDSistGas/src/com/sistgas/controles/SistGas.java
@@ -0,0 +1,12 @@
+package com.sistgas.controles;
+
+import com.sistgas.forms.FormSistGas; 
+import com.sistgas.forms;
+
+public class SistGas {
+
+    public static void main(String[] args) {
+        FormSistGas frmSistGas = new FormSistGas();
+        frmSistGas.setVisible(true);
+    }
+}
